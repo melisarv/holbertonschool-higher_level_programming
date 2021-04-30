d182baee8587143db14c4e562918e443921a9ba0
Melisa Rojas <melisarv@gmail.com>
2021-04-29 23:06:35 -0500

    update program - module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv)
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv)
 
-if (n == 2):
-    print("1 argument:")
-elif (n == 1):
-    print("0 arguments.")
-else:
-    print("{:d} arguments:".format(n - 1))
+    if (n == 2):
+        print("1 argument:")
+    elif (n == 1):
+        print("0 arguments.")
+    else:
+        print("{:d} arguments:".format(n - 1))
 
-for i in range(n - 1):
-    print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
+    for i in range(n - 1):
+        print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
