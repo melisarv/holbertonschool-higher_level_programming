88107b0bf8393dddc52fb86c5802d1d024115ac3
Melisa Rojas <melisarv@gmail.com>
2021-05-05 00:13:02 -0500

    update scripts

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     new_string = ''
-
-    for i in range(len(my_string)):
-        if my_string[i] != 'c' and my_string[i] != 'C':
-            new_string = new_string + my_string[i]
-
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_string += i
     return (new_string)
