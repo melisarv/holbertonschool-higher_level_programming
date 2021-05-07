1c46836510a6bed2f506d80958d68e722ef51560
Melisa Rojas <melisarv@gmail.com>
2021-05-07 01:41:50 -0500

    update script - fix validation none

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary:
+        num = 0
+        for k, v in a_dictionary.items():
+            if num < v:
+                num = v
+                key = k
+        return (key)
+    else:
         return (None)
-    num = 0
-    for k, v in a_dictionary.items():
-        if num < v:
-            num = v
-            key = k
-    return (key)
