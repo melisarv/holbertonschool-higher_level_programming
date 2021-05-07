a9b3d00d56dfb047496d95a881768611149441e0
Melisa Rojas <melisarv@gmail.com>
2021-05-06 23:59:34 -0500

    update script - fix condition

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     n_dictionary = a_dictionary.copy()
-    for k, v in new_dictionary.items():
+    for k, v in n_dictionary.items():
         n_dictionary[k] = v * 2
-    return n_dictionary
+    return (n_dictionary)
