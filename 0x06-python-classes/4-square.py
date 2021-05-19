23237b1fcb49a7812ebed6faffb06d91b5b39d9b
Melisa Rojas <melisarv@gmail.com>
2021-05-19 02:04:03 -0500

    create python scripts

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """definition class square"""
+
+    def __init__(self, size=0):
+        """Initializes square with size"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter value size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter value size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate area and return value"""
+        return self.__size ** 2
