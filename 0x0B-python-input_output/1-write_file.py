b1fc65eb5156787aff2dc1015332d745c530953c
Melisa Rojas <melisarv@gmail.com>
2021-06-03 00:22:10 -0500

    fix-adding comment

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-def write_file(filename="", text=""):
-    '''function that writes a string to a text file and returns
-    the number of characters written'''
+"""Write file function"""
 
-    with open(filename, 'w') as f:
+
+def write_file(filename="", text=""):
+    """function that writes a string to a text file and returns
+    the number of characters written"""
+
+    with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
