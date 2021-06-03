3d8e1daf9024fde1ecc7da58410266bf71295617
Melisa Rojas <melisarv@gmail.com>
2021-06-03 00:15:01 -0500

    fix-update condition

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""Read file function"""
 
 
 def read_file(filename=""):
-    '''function that reads a text file and prints'''
+    """function that reads a text file and prints"""
 
     with open(filename, encoding="utf-8") as f:
-        text = f.read()
-        print(text, end='')
+        for text in f.read():
+            print(text, end='')
