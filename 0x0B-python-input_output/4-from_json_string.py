cbb75293996c9643033fe95b1b06b0b52be7c1e0
Melisa Rojas <melisarv@gmail.com>
2021-06-03 00:42:12 -0500

    fix-adding comment pep8

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
+"""JSON function"""
 import json
 
 
 def from_json_string(my_str):
-    '''function that returns an object represented by JSON string'''
+    """function that returns an object represented by JSON string"""
 
     return json.loads(my_str)
