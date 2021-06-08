7bbfe16e3b4e433da84851d6fb062f1119f3d1e6
Melisa Rojas <melisarv@gmail.com>
2021-06-08 18:29:17 -0500

    create classes, methods and python unit test

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,29 @@
+# Project: 0x0C-python-almost_a_circle
+
+This project is part of the Holberton School curriculum.
+
+In this project you will find the files with solved cases to understand Python programming, Unit testing, serialize and deserialize a Class, write a read JSON file, what is args, kwargs
+
+## Requirements
+## Python Scripts
+* All files will be compiled on Ubuntu 14.04 LTS using python3 (version 3.4.3)
+* All files should end with a new line
+* The first line of all files should be exactly #!/usr/bin/python3
+* The code should use the PEP 8 style (version 1.7.)
+* All files must be executable
+* All modules, clases and functions should be documented
+
+
+## Files - Functions
+
+* tests/
+* models/base.py, models/__init__.py
+* models/rectangle.py
+* models/square.py
+* models/base.py
+
+
+## Authors
+
+Melisa Rojas 
+
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+'''Class Rectangle'''
+from models.base import Base
+
+
+class Rectangle(Base):
+    '''definition Class Rectangle'''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        '''instance initialization method'''
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        '''width getter method'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''width setter method'''
+        self.integer_validator("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        '''height getter method'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''height setter method'''
+        self.integer_validator("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        '''x getter method'''
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        '''x setter method'''
+        self.integer_validator("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        '''y getter method'''
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        '''y setter method'''
+        self.integer_validator("y", value)
+        self.__y = value
+
+    def integer_validator(self, name, value):
+        '''check if value is an integer'''
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if (name == "width" or name == "height") and value <= 0:
+            raise ValueError("{:s} must be > 0".format(name))
+        if (name == "y" or name == "x") and value < 0:
+            raise ValueError("{:s} must be >= 0".format(name))
+
+    def area(self):
+        '''returns the area value of the Rectangle'''
+        return self.__width * self.__height
+
+    def display(self):
+        '''prints the Rectangle instance with the character #'''
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end='')
+            print()
+
+    def __str__(self):
+        '''returns string format'''
+        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
