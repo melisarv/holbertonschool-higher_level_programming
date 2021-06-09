fc6d74770b8f9b312a3affb75102a692b920d753
Melisa Rojas <melisarv@gmail.com>
2021-06-08 23:40:57 -0500

    update the class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 ''' Class Base'''
+import json
+import os
+import csv
 
 
 class Base:
@@ -13,3 +16,19 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''returns the JSON string representation of list_dictionaries'''
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''writes the JSON string representation of list_objs'''
+        my_list = []
+        if list_objs is not None:
+            my_list = [i.to_dictionary() for i in list_objs]
+        with open("{}.json".format(cls.__name__), "w") as f:
+            f.write(cls.to_json_string(my_list))
