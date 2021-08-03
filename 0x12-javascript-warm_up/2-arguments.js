9b0db59f21ccd0ef453d99a85ca5ed79f562df3c
Melisa Rojas <melisarv@gmail.com>
2021-08-02 23:35:35 -0500

    initial commit task 0 - 6

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const argv = process.argv.length;
+if (argv === 2) {
+  console.log('No argument');
+} else if (argv === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
